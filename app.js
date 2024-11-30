01d625ac4a2d8e2242b9f788eb7cbe758ce0e44d
kstaff123 <47585247+kstaff123@users.noreply.github.com>
2024-11-30 13:25:01 -0800

    search functionality

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require("dotenv").config(); // Load environment variables
 const express = require("express");
 const accountRoutes = require("./routes/accountroutes.js");
+const accountRoutesData = require("./routes/accountroutedata.js");
 
 const app = express();
 
 app.use(express.json()); // Parse JSON requests
-app.use("/api", accountRoutes); // Use account routes
+app.use("/api", accountRoutes, accountRoutesData); // Use account routes
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
